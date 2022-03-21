f245b82266f2939edcfc5f5018fcaf5f1df36f19
Đen Đen <huudangntp@gmail.com>
2022-03-21 13:05:23 +0700

    commit luc 13h04 ngay 21/03/2022

--- 1911066045_ThaiHuuDang_BigSchool/Controllers/CoursesController.cs
+++ 1911066045_ThaiHuuDang_BigSchool/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1911066045_ThaiHuuDang_BigSchool.Models;
+using _1911066045_ThaiHuuDang_BigSchool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +10,19 @@ namespace _1911066045_ThaiHuuDang_BigSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
-        //aaaaaaaaa
-        //a
         public ActionResult Create()
         {
-
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
